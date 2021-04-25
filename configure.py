b247b26725e8e94656ca2214038816b24f9677c7
Jacob London <jelondon12@gmail.com>
2021-04-25 10:18:45 -0700

    Simple configure util

---
+++ configure.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import platform
+
+try:
+    fp = open("Makefile", "w")
+except:
+    exit(1)
+
+OS = platform.system()
+if OS in ("Linux", "Darwin"):
+    print("""\
+CC=gcc
+LDFLAGS = -std=c99 -Wall -Iexternal -DPLATFORM_DESKTOP
+CFLAGS = -lraylib -lm -lpthread -ldl -Iinclude -Wall -ggdb
+TARGET = test
+REMOVE = rm -f
+
+.PHONY: clean
+
+SOURCES = $(wildcard src/rlu/*.c) $(wildcard src/*.c) $(wildcard src/modules/*.c)
+OBJECTS = ${SOURCES:c=o}
+
+$(TARGET): $(OBJECTS)
+\t$(CC) -o $@ $^ $(CFLAGS)
+
+clean:
+\t$(REMOVE) $(TARGET) $(OBJECTS)
+""", file=fp)
+
+elif OS == "Windows":
+    print("""\
+CC=C:\\raylib\\tcc\\tcc
+LDFLAGS = C:\\raylib\\raylib\src\\raylib.rc.data -Os -std=c99 -Wall -Iexternal -DPLATFORM_DESKTOP
+CFLAGS = -lmsvcrt -lraylib -lopengl32 -lgdi32 -lwinmm -lkernel32 -lshell32 -luser32 -Wl -O2 -Iinclude/
+TARGET = test.exe
+REMOVE = del
+
+.PHONY: clean
+
+SOURCES = $(wildcard src/rlu/*.c) $(wildcard src/*.c) $(wildcard src/modules/*.c)
+
+$(TARGET): $(SOURCES)
+\t$(CC) -o $@ $^ $(CFLAGS)
+
+clean:
+\t$(REMOVE) $(TARGET)
+""", file=fp)
+
+fp.close()
